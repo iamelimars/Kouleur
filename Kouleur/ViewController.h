7be0d65d696c4ad21ba15369c826705c0f5cc912
Elisha Marshall <iamelimars@yahoo.com>
2016-09-07 15:38:50 -0400

    Added camera and take camera button

--- Kouleur/ViewController.h
+++ Kouleur/ViewController.h
@@ -7,12 +7,34 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
+#import <ImageIO/ImageIO.h>
+#import <QuartzCore/QuartzCore.h>
+@import AVFoundation;
+@import AssetsLibrary;
+@import Photos;
+@import CoreGraphics;
+
 
 @interface ViewController : UIViewController <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIView *introView;
 @property (weak, nonatomic) IBOutlet UIView *cameraView;
 @property (weak, nonatomic) IBOutlet UIScrollView *MainScrollView;
+@property (weak, nonatomic) IBOutlet UIScrollView *CameraScollView;
+@property (weak, nonatomic) IBOutlet UIButton *cameraButton;
+@property (weak, nonatomic) IBOutlet UIView *cameraButtonView;
+- (IBAction)cameraButtonPressed:(id)sender;
 
+@property (nonatomic) AVCaptureSession *session;
+@property (nonatomic) AVCaptureDevice *device;
+@property (nonatomic, retain) AVCaptureStillImageOutput *stillImageOutput;
+@property(nonatomic, strong) UIImageView *firstImage;
+@property(nonatomic, retain) AVCaptureVideoPreviewLayer *previewLayer;
+@property(nonatomic, retain) CALayer *rootLayer;
+@property(weak, nonatomic) NSString *cameraPosition;
+@property(nonatomic, assign) BOOL *imageTaken;
 
 @end
 
--- Kouleur/ViewController.m
+++ Kouleur/ViewController.m
@@ -7,11 +7,10 @@
 //
 
 #import "ViewController.h"
-#import "ViewController1.h"
-#import "ViewController2.h"
-#import "ViewController3.m"
 
 @interface ViewController ()
+
+
 
 @end
 
@@ -20,13 +19,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
     [self.introView setFrame:self.view.frame];
     [self.cameraView setFrame:self.view.frame];
     
+    self.cameraButton.layer.cornerRadius = self.cameraButton.frame.size.height/2;
+    self.cameraButtonView.layer.cornerRadius = self.cameraButtonView.frame.size.height/2;
+}
+
+-(void)viewDidAppear:(BOOL)animated {
     
+    [self createCamera];
     
+}
+
+- (void)createCamera {
     
+    self.session = [[AVCaptureSession alloc]init];
+    self.session.sessionPreset = AVCaptureSessionPresetHigh;
+    self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error = nil;
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:&error];
+    
+    if (!input) {
+        //Handle the error appropriately
+        NSLog(@"ERROR: Trying to open camera: %@", error);
+    }else if ([self.session canAddInput:input]) {
+        [self.session addInput:input];
+    }
+    
+    self.previewLayer = [[AVCaptureVideoPreviewLayer alloc]initWithSession:self.session];
+    [self.previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    
+    self.rootLayer = [[self view]layer];
+    [self.rootLayer setMasksToBounds:YES];
+    [self.previewLayer setFrame:self.view.bounds];
+    [self.rootLayer insertSublayer:self.previewLayer atIndex:0];
+    
+    self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys: AVVideoCodecJPEG, AVVideoCodecKey, nil];
+    [self.stillImageOutput setOutputSettings:outputSettings];
+    [self.session addOutput:self.stillImageOutput];
+    [self.session startRunning];
+
     
 }
 
@@ -35,4 +69,6 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)cameraButtonPressed:(id)sender {
+}
 @end
